65fb3730a8cece806a0babfa153094442668fba1
EugeneGoroschenya <eugene.goroschenya+github@prodexa.com>
2025-02-20 14:32:13 +0300

    feat(pxc): draft multi store support using symlinks

--- src/lib/client.prodexa.ts
+++ src/lib/client.prodexa.ts
@@ -52,7 +52,6 @@ const products: Record<string, Product> = {};
 export const getProducts = async <ThrowOnError extends boolean = false>(
 	options?: Options<GetProductsData, ThrowOnError>,
 ): Promise<RequestResult<GetProductsResponse, GetProductsError, ThrowOnError>> => {
-
 	const response = await fetch(productsApiUrl, {
 		headers: {
 			PXM_USER,
